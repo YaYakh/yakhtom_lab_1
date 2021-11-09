9f28055cd82a6db968318b3cd85dced397f5552d
YaYakh <90177756+YaYakh@users.noreply.github.com>
2021-11-09 03:22:27 +0300

    Add files via upload

--- Source.c
+++ Source.c
@@ -4,60 +4,60 @@
 
 int main()
 {
-	int n = 0;
-	double l = 0.0;
-	double u = 0.0;
-	double sum = 0;
-	int pi = 1;
-	double p = 0;
-	double a = 0;
-	
-	double* mas;
-	mas = (double*)malloc(n * sizeof(double));
-	int* mas2;
-	mas2 = (int*)malloc(n * sizeof(int));
+  int n = 0;
+  double l = 0.0;
+  double u = 0.0;
+  double sum = 0;
+  int pi = 1;
+  double p = 0;
+  double a = 0;
 
-	printf(" n = ");
-	scanf_s(" %d", &n);
-	if (n <= 0) {
-		printf("error n <= 0");
-		return 0;
-	}
+  double* mas;
+  mas = (double*)malloc(n * sizeof(double));
+  int* mas2;
+  mas2 = (int*)malloc(n * sizeof(int));
 
-	printf(" min = ");
-	scanf_s(" %lf", &l);
-	
-	printf(" max = ");
-	scanf_s(" %lf", &u);
+  printf(" n = ");
+  scanf_s(" %d", &n);
+  if (n <= 0) {
+    printf("error n <= 0");
+    return 0;
+  }
 
-	if (l >= u) {
-		printf("error min >= max");
-		return 0;
-	}
+  printf(" min = ");
+  scanf_s(" %lf", &l);
 
-	for (int i = 0; i < n; i++)
-	{
-		p = (((((double)rand()) / RAND_MAX) * (u - l) + l)*1000) ;
-		mas[i] = floor(p) / 1000;
-		mas2[i] = round(modf(mas[i], &a)*1000);
-	}
+  printf(" max = ");
+  scanf_s(" %lf", &u);
 
-	for (int i = 0; i < n; i++)
-	{
-		pi = 1;
-		for (int k = 0; k < n; k++)
-		{
-			if (i == mas2[k])
-				pi = 0;
-				
-		}
-		if (pi)
-			sum = sum + mas[i];
-		else
-			sum = sum - mas[i];
-	}
-	printf("\n\n sum = %.3lf", sum);
+  if (l >= u) {
+    printf("error min >= max");
+    return 0;
+  }
 
-	free(mas);
-	return 0;
+  for (int i = 0; i < n; i++)
+  {
+    p = (((((double)rand()) / RAND_MAX) * (u - l) + l) * 1000);
+    mas[i] = floor(p) / 1000;
+    mas2[i] = round(modf(mas[i], &a) * 1000);
+  }
+
+  for (int i = 0; i < n; i++)
+  {
+    pi = 1;
+    for (int k = 0; k < n; k++)
+    {
+      if (i == mas2[k])
+        pi = 0;
+
+    }
+    if (pi)
+      sum = sum + mas[i];
+    else
+      sum = sum - mas[i];
+  }
+  printf("\n\n sum = %.3lf", sum);
+
+  free(mas);
+  return 0;
 }
